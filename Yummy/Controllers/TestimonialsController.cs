684297c3fbac85a114c743057934b275e236f01d
shasanova <sabinahasanova@outlook.com>
2023-06-05 12:33:17 +0200

    Update Yummy Project

--- Yummy/Controllers/TestimonialsController.cs
+++ Yummy/Controllers/TestimonialsController.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
+using Yummy.DAL;
+using Yummy.Entities;
 
 namespace Yummy.Controllers
 {
     public class TestimonialsController : Controller
     {
+
+        private readonly RelationsYummyDbContext _context;
+
+        public TestimonialsController(RelationsYummyDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Testimonial> testimonials = _context.Testimonials.ToList();
+            return View(testimonials);
         }
+
+
+        public IActionResult Details(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Testimonial testimonial = _context.Testimonials.FirstOrDefault(x => x.Id == id); ;
+            if (testimonial is null) return NotFound();
+            return View(testimonial);
+        }
+
     }
 }
 
